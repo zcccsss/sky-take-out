92a62902742d0fc1be2f3a9f368b215c08499f29
zcccsss <chaoshunchou@qq.com>
2024-02-25 13:39:01 +0800

    苍穹外卖项目初始代码

--- sky-pojo/src/main/java/com/sky/dto/EmployeeLoginDTO.java
+++ sky-pojo/src/main/java/com/sky/dto/EmployeeLoginDTO.java
@@ -7,7 +7,7 @@ import lombok.Data;
 import java.io.Serializable;
 
 @Data
-@ApiModel(description = "员工登录时传递的数据模型")
+@ApiModel("员工登录时传递的数据模型")
 public class EmployeeLoginDTO implements Serializable {
 
     @ApiModelProperty("用户名")
--- sky-server/src/main/java/com/sky/service/EmployeeService.java
+++ sky-server/src/main/java/com/sky/service/EmployeeService.java
@@ -1,7 +1,10 @@
 package com.sky.service;
 
+import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
+import com.sky.dto.EmployeePageQueryDTO;
 import com.sky.entity.Employee;
+import com.sky.result.PageResult;
 
 public interface EmployeeService {
 
@@ -12,4 +15,11 @@ public interface EmployeeService {
      */
     Employee login(EmployeeLoginDTO employeeLoginDTO);
 
+    /**
+     * 新增员工
+     * @param emp
+     */
+    void save(EmployeeDTO emp);
+
+    PageResult pageQuery(EmployeePageQueryDTO employeePageQueryDTO);
 }
